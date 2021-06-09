283e6f77dfcfd107409112fec5a6bb98b3b57702
Amitk3293 <75209120+Amitk3293@users.noreply.github.com>
2021-06-09 16:47:23 +0300

    Update check.sh

--- Check-godaddy/check.sh
+++ Check-godaddy/check.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+
+#use - ./check.sh  2>&1 | tee output.txt | mail -s "godaddy-domains-expiration-time" mail@gmail.com ### For sending out via mail
+
+
 cat domains.txt | while read line
 
 do
 sudo ./check_godaddy -domain $line -warn 30 -crit 14 -key 9Q3PfpNfTQs_JJQ5RXz6BUdSmLNqhizTEo -secret 7w9vfc7XdRRuF1VvVfs5GB
 done
-
-
-
