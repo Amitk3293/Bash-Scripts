fb584e3a6823d7f49d868e74fcb82634f6dff756
Amitk3293 <75209120+Amitk3293@users.noreply.github.com>
2021-06-09 16:49:46 +0300

    Update check.sh

--- Check-godaddy/check.sh
+++ Check-godaddy/check.sh
@@ -7,5 +7,5 @@
 cat domains.txt | while read line
 
 do
-sudo ./check_godaddy -domain $line -warn 30 -crit 14 -key 9Q3PfpNfTQs_JJQ5RXz6BUdSmLNqhizTEo -secret 7w9vfc7XdRRuF1VvVfs5GB
+sudo ./check_godaddy -domain $line -warn 30 -crit 14 -key <Godaddy-API-key> -secret <Godaddy-API-secret>
 done
