25ed104e52f32386de494eb9f5ae808ded848773
Amitk3293 <75209120+Amitk3293@users.noreply.github.com>
2021-06-29 09:54:41 +0300

    Add files via upload

---
+++ remove_queue_older_than_2min.sh
@@ -0,0 +1,6 @@
+/etc/init.d/sendmail stop
+sleep 1
+echo "Cleaing mail queue - only leaving emails in the queue from the last 2 minutes"
+find /var/spool/mqueue/ -mmin +1 -exec rm -f {} \;
+sleep 1
+/etc/init.d/sendmail start
